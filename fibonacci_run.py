f2644bcbec20a0a2faf422fc3a8189ef1cb80cfd
Iain C Docherty <iain.docherty.external@ynap.com>
2019-11-29 09:41:44 +0000

    give better run name

--- fibonacci_run.py
+++ fibonacci_run.py
@@ -9,7 +9,7 @@ index = input('Give the index: ')
 for test in [fib_dynamic, fib_optimize, fib_recursive]:
     try:
         fib = test(float(index))
-        print ("FIB %s = %s" % ( index, fib ))
-    except fib_exception:
+        print ("FIB %s %s = %s" % ( test, index, fib ))
+    except (fib_exception, ValueError):
         print ("Input must be a positive integer")
 
